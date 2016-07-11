fcf5fa34a202acd913c9dc45612781b37bf3a10d
Ronmi Ren <ronmi.ren@gmail.com>
2016-07-12 00:44:30 +0800

    add 的測試碼
    同樣的，這個 commit 裡我也還是不知道測試有沒有寫錯XD
    
    根據 API，這裡寫了三個測試，分別對應正常/重複和參數錯誤的情況

---
+++ cmd/xchg/add_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/Patrolavia/jsonapi"
+)
+
+func TestAddOK(t *testing.T) {
+	h := &add{mgr}
+
+	resp, err := jsonapi.HandlerTest(h.Handle).Post("/api/add", "", `{"when":1468248043,"foreign":100,"local":-100,"code":"USD"}`)
+
+	if err != nil {
+		t.Fatalf("unexpected error occured when testing add: %s", err)
+	}
+
+	if resp.Code != http.StatusOK {
+		t.Fatalf("unexpected error occured when testing add with status code %d: %s", resp.Code, resp.Body.String())
+	}
+
+	if resp.Body == nil {
+		t.Fatal(resp)
+	}
+
+	if str := strings.TrimSpace(resp.Body.String()); str != "null" {
+		t.Errorf("add: returned non-null result: '%s'", str)
+	}
+}
+
+func TestAddDuplicated(t *testing.T) {
+	h := &add{mgr}
+
+	resp, err := jsonapi.HandlerTest(h.Handle).Post("/api/add", "", `{"when":1468248039,"foreign":100,"local":-100,"code":"USD"}`)
+
+	if err != nil {
+		t.Fatalf("unexpected error occured when testing add: %s", err)
+	}
+
+	if resp.Code != http.StatusInternalServerError {
+		t.Fatalf("unexpected status code %d for duplicated record: %s", resp.Code, resp.Body.String())
+	}
+}
+
+func TestAddBadParam(t *testing.T) {
+	h := &add{mgr}
+
+	resp, err := jsonapi.HandlerTest(h.Handle).Post("/api/add", "", `{}`)
+
+	if err != nil {
+		t.Fatalf("unexpected error occured when testing add: %s", err)
+	}
+
+	if resp.Code != http.StatusBadRequest {
+		t.Fatalf("unexpected status code %d for bas request: %s", resp.Code, resp.Body.String())
+	}
+}
