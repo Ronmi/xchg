16cdba288d0ca752a2d09e1788d052db725c1083
Ronmi Ren <ronmi.ren@gmail.com>
2016-07-13 13:23:06 +0800

    讓 server 端也能處理靜態檔案
    接下來 ui 部份要開始發 ajax 了，所以我得從以下三個選項選至少一個
    
    1. 找個好的 js test framework 及 ajax 工具，寫測試驗證程式正確性
    2. 用 node 做個簡單的 mock server
    3. 直接用直的 server
    
    反正後端都寫好了，我選 3

--- cmd/xchg/main.go
+++ cmd/xchg/main.go
@@ -14,8 +14,10 @@ import (
 func main() {
 	var bind string
 	var constr string
+	var uidir string
 	flag.StringVar(&bind, "bind", ":8000", "parameter to pass to http.ListenAndServe")
 	flag.StringVar(&constr, "constr", "file:data.db?cache=shared&mode=rwc", "sqlite connection string")
+	flag.StringVar(&uidir, "ui", "../../ui", "path to ui files, cant be absolute or relative")
 	flag.Parse()
 
 	db, err := sql.Open("sqlite3", constr)
@@ -31,6 +33,14 @@ func main() {
 	jsonapi.HandleFunc("/api/listall", (&listall{mgr}).Handle)
 	jsonapi.HandleFunc("/api/list", (&list{mgr}).Handle)
 	jsonapi.HandleFunc("/api/add", (&add{mgr}).Handle)
+
+	// serve ui files
+	http.Handle("/node_modules/", http.FileServer(http.Dir(uidir)))
+	http.Handle("/css/", http.FileServer(http.Dir(uidir)))
+	http.Handle("/js/", http.FileServer(http.Dir(uidir)))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, uidir+"/index.html")
+	})
 
 	if err := http.ListenAndServe(bind, nil); err != nil {
 		log.Fatalf("Error starting http server: %s", err)
