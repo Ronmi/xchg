561c2365951fb28bda2a4b132a682d9e0c745cdf
Ronmi Ren <ronmi.ren@gmail.com>
2016-07-23 14:54:23 +0800

    完成 OrderForm 的測試
    因為我是用 BDD 的方式寫測試，所以描述都集中在這個 component 對事件的反應。
    這也是為什麼我沒有用 wrapper.state() 去測試每個欄位：只要按 submit 的時
    候它會把每個欄位的資料都傳回來就好了，資料是直接硬抓 input 的值還是綁事
    件進 state 我不需要管，那是實作的細節。
    
    缺點當然是，填欄位的程式又臭又長。不過到是不用擔心改動欄位的事：這些欄位
    我們都有測過，是真實存在的，也是整個 OrderForm 規格的一部份。如果要改規
    格的話，當然整個測試都要重新 review 一遍。跳過規格直接用 wrapper.state()
    反而會養成偷懶的心態，造成測試碼中難以發現的錯誤。

--- ui/test/components/OrderFormTest.tsx
+++ ui/test/components/OrderFormTest.tsx
@@ -11,83 +11,129 @@ let expect = chai.expect;
 
 // helper to emit dom event
 function EV(wrapper: any, ev: string, value: any) {
-    wrapper.simulate(ev, {target: {value: value}});
+    wrapper.simulate(ev, { target: { value: value } });
 }
 
 
 describe("<OrderForm />", () => {
     let d = "2016/07/23 00:00:00";
-    let when = Math.floor(Date.parse(d)/1000);
+    let when = Math.floor(Date.parse(d) / 1000);
     let f = function(data: OrderData) {
     };
     let wrapper = shallow(<OrderForm submitOrder={f} />);
     it("is a form", () => {
-	expect(wrapper.is("form")).to.be.true;
+        expect(wrapper.is("form")).to.be.true;
     });
     it("has an input named when for time", () => {
-	expect(wrapper.find('input[name="when"]')).to.have.length(1);
+        expect(wrapper.find('input[name="when"]')).to.have.length(1);
     });
     it("has an input named local for local currency", () => {
-    	expect(wrapper.find('input[name="local"]')).to.have.length(1);
+        expect(wrapper.find('input[name="local"]')).to.have.length(1);
     });
     it("has an input named foreign for foreign currency", () => {
-    	expect(wrapper.find('input[name="foreign"]')).to.have.length(1);
+        expect(wrapper.find('input[name="foreign"]')).to.have.length(1);
     });
     it("has a currency selector for choosing foreign currency type", () => {
-	expect(wrapper.find('CurrencySelector')).to.have.length(1);
+        expect(wrapper.find('CurrencySelector')).to.have.length(1);
     });
     it("has a submit button to submit the order", () => {
-	expect(wrapper.find('button[type="submit"]')).to.have.length(1);
+        expect(wrapper.find('button[type="submit"]')).to.have.length(1);
     });
 
     describe("submitOrder event", () => {
-	let myspy = sinon.spy(f);
-	let wrapper = shallow(<OrderForm submitOrder={myspy} />);
-	let ok = {
-	    when: when,
-	    local: -3300,
-	    foreign: 100,
-	    code: "USD"
-	};
+        let myspy = sinon.spy(f);
+        let wrapper = shallow(<OrderForm submitOrder={myspy} />);
+        let ok = {
+            when: when,
+            local: -3300,
+            foreign: 100,
+            code: "USD"
+        };
 
-	// simulates filling correct data
+        // simulates filling correct data
 
-	EV(wrapper.find('input[name="when"]'), "change", d);
-	EV(wrapper.find('input[name="local"]'), "change", -3300);
-	EV(wrapper.find('input[name="foreign"]'), "change", 100);
-	wrapper.find('CurrencySelector').prop("codeSelected")("USD");
+        EV(wrapper.find('input[name="when"]'), "change", d);
+        EV(wrapper.find('input[name="local"]'), "change", -3300);
+        EV(wrapper.find('input[name="foreign"]'), "change", 100);
+        wrapper.find('CurrencySelector').prop("codeSelected")("USD");
 
-	// simulates submitting
-	wrapper.find("form").simulate("submit", {preventDefault: function(){}});
-	it("is emitted when submitting correct order", () => {
-	    expect(myspy.called).to.be.true;
-	});
-	it("contains same OrderData as the data in form", () => {
-	    expect(myspy).to.have.been.calledWithMatch(ok);
+        // simulates submitting
+        wrapper.find("form").simulate("submit", { preventDefault: function() { } });
+        it("is emitted when submitting correct order", () => {
+            expect(myspy.called).to.be.true;
+        });
+        it("contains same OrderData as the data in form", () => {
+            expect(myspy).to.have.been.calledWithMatch(ok);
 
-	});
+        });
     });
 
     describe("failing", () => {
-	it("emits an error event when date format is invalid, and no submitCode event", () => {
-	    let e = sinon.spy();
-	    let s = sinon.spy();
-	    let wrapper = shallow(<OrderForm submitOrder={s} error={e} />);
+        let preset = function(w: any) {
+            EV(wrapper.find('input[name="when"]'), "change", d);
+            EV(wrapper.find('input[name="local"]'), "change", -3300);
+            EV(wrapper.find('input[name="foreign"]'), "change", 100);
+            wrapper.find('CurrencySelector').prop("codeSelected")("USD");
+        };
+        it("emits only an error event when date format is invalid", () => {
+            let e = sinon.spy();
+            let s = sinon.spy();
+            let wrapper = shallow(<OrderForm submitOrder={s} error={e} />);
 
-	    EV(wrapper.find('input[name="when"]'), "change", "fail");
-	    EV(wrapper.find('input[name="local"]'), "change", -3300);
-	    EV(wrapper.find('input[name="foreign"]'), "change", 100);
-	    wrapper.find('CurrencySelector').prop("codeSelected")("USD");
-	    wrapper.find("form").simulate("submit", {preventDefault: function(){}});
+            preset(wrapper);
+            EV(wrapper.find('input[name="when"]'), "change", "fail");
+            wrapper.find("form").simulate("submit", { preventDefault: function() { } });
 
-	    expect(e).to.be.calledOnce;
-	    expect(s).not.to.be.called;
-	});
-	it("emits an error event when foreign currency format is invalid", () => {
-	});
-	it("emits an error event when local currency format is invalid", () => {
-	});
-	it("emits an error event when both local and foreign currency are positive");
-	it("emits an error event when both local and foreign currency are negative");
+            expect(e).to.be.calledOnce;
+            expect(s).not.to.be.called;
+        });
+        it("emits only an error event when foreign currency format is invalid", () => {
+            let e = sinon.spy();
+            let s = sinon.spy();
+            let wrapper = shallow(<OrderForm submitOrder={s} error={e} />);
+
+            preset(wrapper);
+            EV(wrapper.find('input[name="foreign"]'), "change", "fail");
+            wrapper.find("form").simulate("submit", { preventDefault: function() { } });
+
+            expect(e).to.be.calledOnce;
+            expect(s).not.to.be.called;
+        });
+        it("emits only an error event when local currency format is invalid", () => {
+            let e = sinon.spy();
+            let s = sinon.spy();
+            let wrapper = shallow(<OrderForm submitOrder={s} error={e} />);
+
+            preset(wrapper);
+            EV(wrapper.find('input[name="local"]'), "change", "fail");
+            wrapper.find("form").simulate("submit", { preventDefault: function() { } });
+
+            expect(e).to.be.calledOnce;
+            expect(s).not.to.be.called;
+        });
+        it("emits only an error event when both local and foreign currency are positive", () => {
+            let e = sinon.spy();
+            let s = sinon.spy();
+            let wrapper = shallow(<OrderForm submitOrder={s} error={e} />);
+
+            preset(wrapper);
+            EV(wrapper.find('input[name="local"]'), "change", 3300);
+            wrapper.find("form").simulate("submit", { preventDefault: function() { } });
+
+            expect(e).to.be.calledOnce;
+            expect(s).not.to.be.called;
+        });
+        it("emits only an error event when both local and foreign currency are negative", () => {
+            let e = sinon.spy();
+            let s = sinon.spy();
+            let wrapper = shallow(<OrderForm submitOrder={s} error={e} />);
+
+            preset(wrapper);
+            EV(wrapper.find('input[name="local"]'), "change", 3300);
+            wrapper.find("form").simulate("submit", { preventDefault: function() { } });
+
+            expect(e).to.be.calledOnce;
+            expect(s).not.to.be.called;
+        });
     });
 });
