4bf358442847025b786f9d5a53f1d00d353bd4c9
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-02 20:59:17 +0800

    處理了 karma 卻忘了處理 webpack

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -28,8 +28,13 @@ module.exports = {
 
   module: {
     loaders: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+	test: /\.tsx?$/,
+	loaders: [
+	  "babel-loader?presets[]=es2015",
+	  "ts-loader",
+	],
+      },
       {
 	test: /\.js/,
 	loader: "babel-loader",
