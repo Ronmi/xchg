094495eef3a3ca6633989ab027264689cc93dba8
Ronmi Ren <ronmi.ren@gmail.com>
2016-08-02 22:15:11 +0800

    把 App 的測試通通改寫成 async/await 形式

--- ui/test/AppTest.tsx
+++ ui/test/AppTest.tsx
@@ -1,5 +1,7 @@
 /// <reference path="../typings/globals/mocha/index.d.ts" />
 /// <reference path="../typings/globals/chai/index.d.ts" />
+/// <reference path="../typings/globals/promises-a-plus/index.d.ts" />
+/// <reference path="../typings/globals/chai-as-promised/index.d.ts" />
 
 import * as React from "react";
 import { shallow, ShallowWrapper } from "enzyme";
@@ -9,16 +11,6 @@ import FakeAPI from "./FakeAPI";
 
 // use global chai since karma-chai-sinon registers to global scope
 let expect = chai.expect;
-let check = function(done: any, cb: Function): () => void {
-    return () => {
-        try {
-            cb();
-            done();
-        } catch (e) {
-            done(e);
-        }
-    };
-};
 
 let helper = {
     emitPincode: function(wrapper: any, pin?: string): Promise<void> {
@@ -63,153 +55,101 @@ describe("<App />", () => {
     });
 
     describe("logic", () => {
-        it("authenticates user on submitPincode event", (done) => {
+        it("authenticates user on submitPincode event", async () => {
             let api = new FakeAPI();
             api.will.success();
             sinon.spy(api, "Auth");
             let wrapper = shallow(<App api={api} alert={() => { } } />);
-            helper.emitPincode(wrapper).then(
-                check(done, () => {
-                    expect(api.Auth).has.been.calledOnce;
-                    expect(wrapper.state("authed")).to.be.true;
-                }),
-                () => { done(new Error("Unexpected error occurs with api.Auth")); }
-            );
+            await helper.emitPincode(wrapper);
+            expect(api.Auth).has.been.calledOnce;
+            expect(wrapper.state("authed")).to.be.true;
         });
-        it("stays at login form if failed to login", (done) => {
+        it("stays at login form if failed to login", async () => {
             let api = new FakeAPI();
             api.will.fail();
             sinon.spy(api, "Auth");
             let wrapper = shallow(<App api={api} alert={() => { } } />);
-            helper.emitPincode(wrapper).then(
-                () => { done(new Error("Unexpected api success with api.Auth")); },
-                check(done, () => {
-                    expect(wrapper.state("authed")).to.be.false;
-                })
-            );
+
+	    await expect(helper.emitPincode(wrapper)).to.be.rejected;
+            expect(wrapper.state("authed")).to.be.false;
         });
-        it("updates order list data after successful authenticating", (done) => {
+        it("updates order list data after successful authenticating", async () => {
             let api = new FakeAPI();
             api.will.success();
             api.fakeData = [{ when: 1, foreign: 1, local: -1, code: "USD" }];
             sinon.spy(api, "ListAll");
             let wrapper = shallow(<App api={api} alert={() => { } } />);
-            helper.emitPincode(wrapper).then(
-                check(done, () => {
-                    expect(api.ListAll).has.been.calledOnce;
-                    expect(wrapper.state("data")).to.deep.equal(api.fakeData);
-                }),
-                () => { done(new Error("Unexpected error occurs with api.Auth")); }
-            );
+            await helper.emitPincode(wrapper);
+            expect(api.ListAll).has.been.calledOnce;
+            expect(wrapper.state("data")).to.deep.equal(api.fakeData);
         });
-        it("adds a new order to backend on submitOrder event", (done) => {
+        it("adds a new order to backend on submitOrder event", async () => {
             let api = new FakeAPI();
             api.will.success();
             sinon.spy(api, "Add");
             let wrapper = shallow(<App api={api} alert={() => { } } />);
-            helper.emitPincode(wrapper).then(
-                check(done, () => {
-                    wrapper.update();
-                    let check = function() {
-                        expect(api.Add).has.been.calledOnce;
-                    };
-                    helper.emitOrder(wrapper, { when: 1, local: -1, foreign: 1, code: "USD" }).then(
-                        check, check
-                    );
-                }),
-                () => { done(new Error("Unexpected error occurs with api.Auth")); }
-            );
+            await helper.emitPincode(wrapper); // emulate login
+            wrapper.update();
+
+            await helper.emitOrder(wrapper, { when: 1, local: -1, foreign: 1, code: "USD" });
+            expect(api.Add).has.been.calledOnce;
         });
-        it("updates order list data after new data added", (done) => {
+        it("updates order list data after new data added", async () => {
             let order = { when: 1, local: -1, foreign: 1, code: "USD" }
             let api = new FakeAPI();
             api.will.success();
             let wrapper = shallow(<App api={api} alert={() => { } } />);
-            helper.emitPincode(wrapper).then(
-                () => {
-                    wrapper.update();
-                    api.fakeData = [order];
-                    helper.emitOrder(wrapper, order).then(
-                        check(done, () => {
-                            expect(api.fakeData).to.deep.equal(wrapper.state("data"));
-                        }),
-                        () => { done(new Error("Unexpected error occurs with api.Add")); }
-                    );
-                },
-                () => { done(new Error("Unexpected error occurs with api.Auth")); }
-            );
+            await helper.emitPincode(wrapper); // emulate login
+            wrapper.update();
+
+            api.fakeData = [order];
+            await helper.emitOrder(wrapper, order);
+            expect(api.fakeData).to.deep.equal(wrapper.state("data"));
         });
-	it("will not update order list if new data does not belong to current category", (done) => {
+	it("will not update order list if new data does not belong to current category", async () => {
             let order = { when: 1, local: -1, foreign: 1, code: "USD" }
             let api = new FakeAPI();
             api.will.success();
 	    api.fakeData = [{ when: 1, local: -1, foreign: 1, code: "JPY" }];
             let wrapper = shallow(<App api={api} alert={() => { } } />);
 
-	    // login
-            helper.emitPincode(wrapper).then(
-		() => {
-		    wrapper.update();
-		    // select to JPY
-		    helper.emitCode(wrapper, "JPY").then(
-			() => {
-			    wrapper.update();
-			    helper.emitOrder(wrapper, order).then(
-				check(done, () => {
-				    wrapper.update();
-				    expect(api.fakeData).to.deep.equal(wrapper.state("data"));
-				}),
-				() => { done(new Error("Unexpected error occurs with api.Add")); }
-			    );
-			},
-			() => { done(new Error("Unexpected error occurs with emitting codeSelected")); }
-		    );
-		},
-                () => { done(new Error("Unexpected error occurs with api.Auth")); }
-            );
+            await helper.emitPincode(wrapper); // emulate login
+	    wrapper.update();
+	    await helper.emitCode(wrapper, "JPY"); // select JPY
+	    wrapper.update();
+
+	    await helper.emitOrder(wrapper, order);
+	    wrapper.update();
+	    expect(api.fakeData).to.deep.equal(wrapper.state("data"));
 	});
-        it("remains old order list data if failed to add data", (done) => {
+        it("remains old order list data if failed to add data", async () => {
             let order = { when: 1, local: -1, foreign: 1, code: "USD" }
             let api = new FakeAPI();
             api.will.success();
             let wrapper = shallow(<App api={api} alert={() => { } } />);
-            helper.emitPincode(wrapper).then(
-                () => {
-                    wrapper.update();
-                    api.fakeData = [order];
-                    api.will.fail();
-                    helper.emitOrder(wrapper, order).then(
-                        () => { done(new Error("Unexpected success with api.Add")); },
-                        check(done, () => {
-                            expect(wrapper.state("data")).to.have.length(0);
-                        })
-                    );
-                },
-                () => { done(new Error("Unexpected error occurs with api.Auth")); }
-            );
+            await helper.emitPincode(wrapper); // emulate login
+            wrapper.update();
+
+            api.fakeData = [order];
+            api.will.fail();
+            await expect(helper.emitOrder(wrapper, order)).to.be.rejected;
+            expect(wrapper.state("data")).to.have.length(0);
         });
-        it("updates order list data according to codeSelected event", (done) => {
+        it("updates order list data according to codeSelected event", async () => {
             const code = "USD";
             let order = { when: 1, local: -1, foreign: 1, code: code }
             let api = new FakeAPI();
             sinon.spy(api, "List");
             api.will.success();
             let wrapper = shallow(<App api={api} alert={() => { } } />);
-            helper.emitPincode(wrapper).then(
-                () => {
-                    wrapper.update();
-                    api.fakeData = [order];
-                    helper.emitCode(wrapper, code).then(
-                        check(done, () => {
-                            expect(api.List).calledOnce;
-                            expect(api.List).calledWith(code);
-                        }),
-                        () => { done(new Error("Unexpected error occured with api.List")); }
-                    );
-                },
-                () => { done(new Error("Unexpected error occurs with api.Auth")); }
-            );
-        });
+            await helper.emitPincode(wrapper); // emulate login
+            wrapper.update();
+
+            api.fakeData = [order];
+            await helper.emitCode(wrapper, code);
+            expect(api.List).calledOnce;
+            expect(api.List).calledWith(code);
+	});
     });
 
     describe("error handling", () => {
@@ -219,20 +159,16 @@ describe("<App />", () => {
 	    helper.emitPincodeError(wrapper);
 	    expect(spy).has.been.calledOnce;
 	});
-	it("shows message when order format error", (done) => {
+	it("shows message when order format error", async () => {
 	    let spy = sinon.spy();
 	    let api = new FakeAPI();
 	    let wrapper = shallow(<App api={api} alert={spy} />);
 
-	    // login
-	    helper.emitPincode(wrapper).then(
-		check(done, () => {
-		    wrapper.update();
-		    helper.emitOrderError(wrapper);
-		    expect(spy).has.been.calledOnce;
-		}),
-		() => { done(new Error("Unexpected error occurs with api.Auth")); }
-	    );
+	    await helper.emitPincode(wrapper); // emulate login
+	    wrapper.update();
+
+	    await helper.emitOrderError(wrapper);
+	    expect(spy).has.been.calledOnce;
 	});
     });
 });
